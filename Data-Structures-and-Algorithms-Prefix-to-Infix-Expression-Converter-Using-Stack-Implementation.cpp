53c828701e1a7fe989a17c6dfb7b75493419c63d
waqar-nadeem <waqarnadeem088@gmail.com>
2025-10-22 23:54:40 +0500

    Create Data-Structures-and-Algorithms-Prefix-to-Infix-Expression-Converter-Using-Stack-Implementation.cpp

---
+++ Data-Structures-and-Algorithms-Prefix-to-Infix-Expression-Converter-Using-Stack-Implementation.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+#define MAX 100
+
+class Stack {
+    string arr[MAX];
+    int top;
+public:
+    Stack() { top = -1; }
+    void push(string str) { arr[++top] = str; }
+    string pop() { return arr[top--]; }
+};
+
+bool isOperator(char c) {
+    return (c == '+' || c == '-' || c == '*' || c == '/');
+}
+
+string prefixToInfix(string prefix) {
+    Stack s;
+    for (int i = prefix.length() - 1; i >= 0; i--) {
+        char c = prefix[i];
+        if (isalpha(c)) {
+            s.push(string(1, c));  // push operand
+        } else if (isOperator(c)) {
+            string op1 = s.pop();
+            string op2 = s.pop();
+            string temp = "(" + op1 + c + op2 + ")";
+            s.push(temp);
+        }
+    }
+    return s.pop();
+}
+
+int main() {
+    string prefix;
+    cout << "Enter Prefix Expression: ";
+    cin >> prefix;
+
+    string infix = prefixToInfix(prefix);
+    cout << "Infix Expression: " << infix << endl;
+    return 0;
+}
